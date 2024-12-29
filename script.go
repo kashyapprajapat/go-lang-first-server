75ce3616075c5b31ea8eec3393b1cacf0ef8e3e1
satyam prajapati <satyamprajapati1979@gmail.com>
2024-12-29 20:49:39 +0530

    adding port variabel

--- script.go
+++ script.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -11,8 +12,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Respond with a simple message
 	fmt.Fprintf(w, "Hello, World!")
-} 
-
+}
 
 func SumFrom1To10Million(w http.ResponseWriter, r *http.Request) {
 	// Calculate the sum
@@ -27,8 +27,6 @@ func SumFrom1To10Million(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("The total sum from 1 to 10 million is: " + strconv.Itoa(total)))
 }
 
-
-
 func SumFrom1To10Crore(w http.ResponseWriter, r *http.Request) {
 	// Calculate the sum
 	total := 0
@@ -41,13 +39,21 @@ func SumFrom1To10Crore(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("The total sum from 1 to 10 crore is: " + strconv.Itoa(total)))
 }
+
 func main() {
-	// Set up a route for the root URL
+	// Use the PORT environment variable or default to 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		fmt.Println("PORT environment variable not set. Defaulting to :8080")
+	}
+
+	// Set up routes
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/loop", SumFrom1To10Million)
 	http.HandleFunc("/loop2", SumFrom1To10Crore)
 
-	// Start the server and listen on port 8080
-	fmt.Println("Server starting at :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Start the server
+	fmt.Printf("Server starting on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
